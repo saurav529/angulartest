b64a74a4b3a8ee1b658e55b28b4bb5e94133c594
saurav529 <saurav.bhandari@yahoo.com>
2020-03-17 00:24:31 +0545

    Pushed Changes on 03/17/20 at 12:24:31 AM

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
-#NOW = $(date +"Pushed Changes on %D at %r")
+#rm -rf docs
 #ng build --prod --output-path docs --base-href /angulartest/
-#git add src/*
-#git add docs
-#git add docs/*
+git add src/*
+git add docs
+git add docs/*
+git add deploy.sh
 git commit -m "$(date +"Pushed Changes on %D at %r")"
-#date +"Pushed Changes on %D at %r"
+git push origin master
