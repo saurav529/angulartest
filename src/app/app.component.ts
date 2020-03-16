6fe1d67f43760d8e7f7f1dbc3daabf06637161d9
saurav529 <saurav.bhandari@yahoo.com>
2020-03-17 00:13:51 +0545

    Pushed Changes on 03/17/20 at 12:13:51 AM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angulartest';
+
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          console.log(event.urlAfterRedirects);
+          gtag('config', 'G-ZDZBPHGED8', {page_path: event.urlAfterRedirects});
+        }
+      }
+    );
+  }
 }
