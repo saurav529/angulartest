c0d7be13d5688f1ed7868d022891813e9e322048
saurav529 <saurav.bhandari@yahoo.com>
2020-03-16 23:39:46 +0545

    Initial branch

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#NOW = $(date +"Pushed Changes on %D at %r")
+#ng build --prod --output-path docs --base-href /angulartest/
+#git add src/*
+#git add docs
+#git add docs/*
+git commit -m "$(date +"Pushed Changes on %D at %r")"
+#date +"Pushed Changes on %D at %r"
