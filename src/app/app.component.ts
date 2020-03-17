9cc20a5779ca4d7a7d8a8f549621a6685f2c3983
saurav529 <saurav.bhandari@yahoo.com>
2020-03-17 06:48:43 +0545

    Pushed Changes on 03/17/20 at 06:48:43 AM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,27 @@ declare let gtag: Function;
 })
 export class AppComponent {
   title = 'angulartest';
+  pageTitle;
 
   constructor(public router: Router) {
     this.router.events.subscribe(event => {
         if (event instanceof NavigationEnd) {
-          console.log(event.urlAfterRedirects);
-          gtag('config', 'UA-103072673-1', {page_path: event.urlAfterRedirects});
+          switch (event.urlAfterRedirects) {
+            case '/':
+              this.pageTitle = 'HomePage';
+              break;
+            case '/test':
+              this.pageTitle = 'Test Page';
+              break;
+            case '/gallery':
+              this.pageTitle = 'Image Gallery';
+              break;
+            case '/login':
+              this.pageTitle = 'Login Page';
+              break;
+          }
+          console.log(`page_path: ${event.urlAfterRedirects}, page_title: ${this.pageTitle}`)
+          gtag('config', 'UA-103072673-1', {page_path: event.urlAfterRedirects, page_title: this.pageTitle});
         }
       }
     );
